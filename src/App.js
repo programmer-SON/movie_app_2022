189baf16c82ad7657f67122d6c98c4b67193c829
programmer-SON <sjw3957@gmail.com>
2022-06-18 20:20:45 +0900

    10.0 Creating your first React Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Potato from "./Potato";
 
 function App() {
   return (
-    <div className="App">Helllo!!! </div>
+    <div className="App">
+      <h1>Helllo!!!</h1>
+      <Potato />
+     </div>
   );
 }
 
