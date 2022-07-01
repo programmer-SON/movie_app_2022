3e1821e4bfdb7841e49241fcdced21648a5b7aa8
programmer-SON <sjw3957@gmail.com>
2022-07-01 21:30:56 +0900

    #11.2 Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import PropTypes from "prop-types";
 
 class App extends React.Component{
+
+  // 호출 1
+  constructor(props){
+    super(props)
+    console.log("hello");
+  }
+
   state = {
     count: 0
   };
 
-  /**
-   * setState 호출시 render가 새로 호출
-   */
   add = () => {
     this.setState(current => ({count: current.count + 1}));
   };
@@ -16,7 +20,24 @@ class App extends React.Component{
     this.setState(current => ({count: current.count - 1}));
   };
 
+  // 호출 3
+  componentDidMount(){
+    console.log("component rendered");
+  }
+
+  // 수정시 호출
+  componentDidUpdate(){
+    console.log("I just updated");
+  }
+
+  // 컴포넌트 삭제시 호출 (수정이 일어나면 기존 컴포넌트가 삭제되는것으로 추정)
+  componentWillUnmount(){
+    console.log("Goodbye, cruel world");
+  }
+
+  // 호출 2
   render(){
+    console.log("I am rendering");
     return( 
     <div>
       <h1>The number is: {this.state.count}</h1>
