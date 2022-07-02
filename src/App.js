d2a1bf398abe0deb70fbf6d61136a9129b2954f9
programmer-SON <sjw3957@gmail.com>
2022-07-02 22:03:34 +0900

    #12.0 Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import axios from 'axios';
 
 class App extends React.Component{
 
@@ -8,10 +8,12 @@ class App extends React.Component{
     movies: []
   };
 
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  }
+
   componentDidMount(){    
-    setTimeout(() => {
-      this.setState({isLoading: false});
-    }, 6000);
+    this.getMovies();  
   }
 
   render(){    
