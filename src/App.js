b5ace6372bcb88e27613657b747c3d8d35305279
programmer-SON <sjw3957@gmail.com>
2022-07-02 20:38:26 +0900

    #11.3 Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -3,46 +3,22 @@ import PropTypes from "prop-types";
 
 class App extends React.Component{
 
-  // 호출 1
-  constructor(props){
-    super(props)
-    console.log("hello");
-  }
-
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
 
-  add = () => {
-    this.setState(current => ({count: current.count + 1}));
-  };
-  minus = () => {
-    this.setState(current => ({count: current.count - 1}));
-  };
-
-  // 호출 3
-  componentDidMount(){
-    console.log("component rendered");
+  componentDidMount(){    
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 6000);
   }
 
-  // 수정시 호출
-  componentDidUpdate(){
-    console.log("I just updated");
-  }
-
-  // 컴포넌트 삭제시 호출 (수정이 일어나면 기존 컴포넌트가 삭제되는것으로 추정)
-  componentWillUnmount(){
-    console.log("Goodbye, cruel world");
-  }
-
-  // 호출 2
-  render(){
-    console.log("I am rendering");
+  render(){    
+    const {isLoading} = this.state;
     return( 
     <div>
-      <h1>The number is: {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
+      {isLoading ? "Loading..." : "We are ready"}
     </div>
       );
   }
