1e4274da300b93f262a6bcd3df7e0ae5b7998cac
programmer-SON <sjw3957@gmail.com>
2022-07-17 15:04:44 +0900

    #14.2 Building the Navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { HashRouter, Route } from "react-router-dom";
+import { BrowserRouter,  Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Navigation from "./components/Navigation";
+import "./App.css"
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
+      <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
